8610c4e5095c48db647ecd5fff54fabdc8892c45
John Sundh <john.sundh@scilifelab.se>
2026-02-12 12:57:56 +0100

    Update

--- read_ids.py
+++ read_ids.py
@@ -2,7 +2,7 @@
 
 import polars as pl
 
-df = pl.read_csv("ids.txt", separator=" ", has_header=False)
+df = pl.read_csv("ids.txt", separator=" ", has_header=False).filter(pl.col("column_1").is_not_null())
 assert(df.height==2)
 assert(df.width==3)
 df.write_csv("ids.csv", separator=",")
